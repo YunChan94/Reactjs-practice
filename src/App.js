da01ccff69fe1167ada0dcec4f6aca9553cf0704
YunChan94 <juleefang94@gmail.com>
2022-08-22 16:59:14 +0700

    update code usememo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState, useCallback } from "react";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowparagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowparagraph((prevShowparagraph) => !prevShowparagraph);
+    }
+  }, [allowToggle]);
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow toggle</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
