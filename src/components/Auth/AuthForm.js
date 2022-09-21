4fba09039f9f749277e046394b29cab5a7b87a8d
YunChan <juleefang94@gmail.com>
2022-09-21 23:11:18 +0700

    done

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,11 +1,14 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
-
+import AuthContext from "../../store/auth-context";
 const AuthForm = () => {
+  const history = useHistory();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
+  const authCtx = useContext(AuthContext);
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -58,7 +61,13 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        console.log(data);
+        //lấy giá trị expiresIn trả về từ data (second=>milisecond)
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+        authCtx.login(data.idToken, expirationTime.toISOString());
+        // chuyển sang trang khác
+        history.replace("/");
       })
       .catch((err) => {
         alert(err.message);
